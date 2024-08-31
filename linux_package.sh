ef86a00d1b06c368a07d71879d02911d9743e84c
Santtu Keskinen <santtu.keskinen@gmail.com>
2024-08-31 17:14:05 -0400

    Linux onefile packaged in .tar

--- linux_package.sh
+++ linux_package.sh
@@ -2,10 +2,10 @@
 
 pushd "$(dirname "$0")"
 
-rm -rf dist
+rm -rf build dist
 
 pyinstaller --distpath ./dist --workpath ./build --onefile -n smartcut smartcut/__main__.py
 
-tar -czvf "dist/smartcut_linux.tar.gz" dist/smartcut
-
+tar -czvf "dist/smartcut_linux.tar" -C dist smartcut
+./dist/smartcut --version
 popd
