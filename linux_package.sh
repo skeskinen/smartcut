7913444477cde8697f08dd2e91e8a27dd553d550
Santtu Keskinen <santtu.keskinen@gmail.com>
2024-08-31 23:41:40 +0300

    Place linux binary in .tar.gz to preserve file mode

--- linux_package.sh
+++ linux_package.sh
@@ -4,6 +4,8 @@ pushd "$(dirname "$0")"
 
 rm -rf dist
 
-pyinstaller --distpath ./dist --workpath ./build --onefile -n smartcut_linux smartcut/__main__.py
+pyinstaller --distpath ./dist --workpath ./build --onefile -n smartcut smartcut/__main__.py
+
+tar -czvf "dist/smartcut_linux.tar.gz" dist/smartcut
 
 popd
